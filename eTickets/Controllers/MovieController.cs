2ff31974ad789f9684f6301a9f8f9b15910b95f1
payam khavari far <payam.khavarifar83@gmail.com>
2025-02-26 08:49:52 +0330

    #the Third Changes

--- eTickets/Controllers/MovieController.cs
+++ eTickets/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Enums;
+using eTickets.Data.Services;
 using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,53 +9,44 @@ namespace eTickets.Controllers
 {
     public class MovieController : Controller
     {
-        private readonly AppDbContext _dbContex;
-        public MovieController(AppDbContext dbContext)
+        private readonly IMovieService _service;
+        public MovieController(IMovieService service)
         {
-            _dbContex = dbContext;
+            _service = service;
         }
         public IActionResult Index()
         {
-            
-          
-                List<Movie> ListOfMovies = new List<Movie>()
-               {
-            new Movie()
-            {
-                ImageUrl = "/Images/Ad Astra DVD Cover.jpg" ,
-                Description = "ghjk",
-                MovieCategory = MovieCategory.Horror,
-                Name = "koroush",
-                CinemaId = 1 ,
-                ProducerId = 1
-            },
-            new Movie()
-            {
-                ImageUrl ="/Images/Primal DVD Cover.jpg" ,
-                Description = "ghjk",
-                MovieCategory = MovieCategory.Action,
-                Name = "koroush 2",
-                CinemaId = 1 ,
-                ProducerId= 2
-            },
-            new Movie()
-            {
-                ImageUrl ="/Images/Primal DVD Cover.jpg" ,
-                Description = "ghjk",
-                MovieCategory = MovieCategory.Cartoon,
-                Name = "koroush 4",
-                CinemaId = 1 ,
-                ProducerId= 1
-            }
-        };
-
-            //_dbContex.Movies.AddRange(ListOfMovies);
-            //_dbContex.SaveChanges();
-
-
-            var movies = _dbContex.Movies.Include(x => x.Cinema).ToList(); // دریافت فیلم‌های ذخیره‌شده
+            var movies = _service.GetMovies();
             return View(movies);
         }
+        public IActionResult Details(int id)
+        {
+           var movie =  _service.GetMovie(id);
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var result = _service.GetMovie(id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(result);
+            }
+        }
+
+        [HttpPost]
+       public IActionResult Edit(int id,Movie movie)
+        {
+            var result = _service.Update(movie);
+            return RedirectToAction("Index");
+        }
+
+
 
     }
 }
---
+++ eTickets/Data/Services/IActorService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorService
+    {
+        IEnumerable<Actor> GetAllActors();
+        Actor GetActorById(int id);
+        void AddActor(Actor actor);
+        void UpdateActor(Actor actor);
+        void DeleteActor(int id);
+    }
+}
